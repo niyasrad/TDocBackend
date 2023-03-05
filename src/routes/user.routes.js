545fa56a32684a6961b121c5678d63a038bbb7bf
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-03-05 21:16:09 +0530

    [Niyas] Added Default Tasks

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,27 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const Task = require('../models/task');
+
+const tasks = [
+    {
+        title: "Getting Started",
+        description: "Learn how to use T-DOC. application and create tasks",
+        task: "To get started with T-DOC., simply open the application and click on the 'Create Task' button. Fill in the details for your task and click on 'Save'. Your task will now be added to the task list for you to manage and keep track of.",
+        due: new Date(),
+        priority: "HIGH",
+        category: "Personal"
+    },
+    {
+        title: "Use Categories",
+        description: "Learn how to create categories and add tasks to them",
+        task: "T-DOC. makes it easy for you to organize your tasks by category. To create a new category, simply click on the 'Create Category' button and fill in the details. Once you have created a category, you can view all tasks assigned to that category by clicking on the category name. To add a new task to a category, click on the 'Create Task' button within the category view and fill in the details for your task.",
+        due: new Date(),
+        priority: "HIGH",
+        category: "Personal",
+    }
+]
+
 
 router.post('/create', async(req, res) => {
     try {
@@ -15,6 +36,20 @@ router.post('/create', async(req, res) => {
         })
         await user.save();
         const token = await user.generateAuthToken();
+
+        
+        tasks.map(async (task) => {
+            const newTask = new Task({
+                title: task.title,
+                description: task.description,
+                task: task.task,
+                due: task.due,
+                priority: task.priority,
+                category: task.category,
+                accountid: user._id
+            })
+            await newTask.save()
+        })
         return res.status(200).json({
             token: token,
             accountid: user._id
