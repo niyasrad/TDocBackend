4c546ae30ce0aa839e2d0af053ca2ef9cc67ea57
Niyas Hameed <84234554+niyasrad@users.noreply.github.com>
2023-03-20 23:26:51 +0530

    [Niyas] NodeMailer Active

--- index.js
+++ index.js
@@ -8,9 +8,17 @@ const combineMiddleware = require('./src/util');
 const authMiddleware = require('./src/util/auth');
 
 const app = express();
+const cron = require('node-cron')
+const nodemailer = require('nodemailer')
 
 combineMiddleware(app);
 combineRoutes(app);
+
+app.get('/status', async(req, res) => {
+    return res.status(200).json({
+        message: "Up and Running!"
+    });
+})
 
 app.use(authMiddleware);
 app.get('/', async(req, res) => {
@@ -18,6 +26,59 @@ app.get('/', async(req, res) => {
         username: req.user.username
     });
 })
+
+
+const Task = require('./src/models/task')
+const User = require('./src/models/user')
+
+
+const transporter = nodemailer.createTransport({
+    service: 'hotmail', 
+    auth : {
+        user: process.env.EMAIL,
+        pass: process.env.PPWORD
+    }
+})
+
+cron.schedule('0 0 * * *', async () => {
+    console.log('---------------------');
+    console.log('Running Cron Process');
+
+    let onDueTasks = await Task.aggregate([
+        {
+            $match: { due: { $lte: new Date() }, done: false, bogus: false }
+        },
+        {
+            $group: { _id: "$accountid", tasks: { $push: '$$ROOT' }}
+        },
+        {
+            $project: { _id: 0, accountid:"$_id", tasks: 1 }
+        },
+        {
+            $sort: { accountid: 1 }
+        }
+    ])
+    for (accounts of onDueTasks) {
+        let user = await User.findById(accounts.accountid)
+        if (user){
+            console.log(user)
+            transporter.sendMail({
+                from: {
+                    name: 'TDOC - Niyas',
+                    address: process.env.EMAIL,
+                },
+                to: user.email,
+                subject: `Tasks Due for ${user.username}`,
+                text: 'T-Doc Tasks Notification', 
+                html: `<h1>Tasks Due: ${accounts.tasks.length} </h1><br><br>` + accounts.tasks.map(task => `<h2>${task.title} : ${task.priority === "LOW" ? "Low Priority" : "High Priority"}</h2>`).join("")
+            }, (error, info) => {
+                if (error) console.log(error);
+                else console.log('Email sent: ' + info.response);
+            });
+        }
+    }
+    
+});
 
 const PORT = process.env.PORT || 8080;
 
